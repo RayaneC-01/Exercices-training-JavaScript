26f5737379a65a8a1b633f3245d0123b7e471dc5
Rayane <rchaabane20@gmail.com>
2024-05-17 00:29:16 +0200

    Added directories

---
+++ Jour2_Manipulation DOM/script.js
@@ -0,0 +1,19 @@
+// Jour 2
+/*
+1.Créer une page HTML avec un bouton. En utilisant JavaScript, ajoutez un événement clic au bouton qui affiche une boîte de dialogue avec un message.
+2.Écrire un script qui change la couleur de fond d'un élément de page lorsque l'utilisateur passe sa souris dessus.
+3.Créer un formulaire simple avec un champ de texte et un bouton. À la soumission du formulaire, récupérez la valeur du champ de texte et affichez-la dans la console.
+4.Construire une liste à puces à partir d'un tableau d'éléments à l'aide de JavaScript.
+5.Créer un carrousel d'images simple qui change automatiquement toutes les quelques secondes.
+*/
+
+// Button Event
+// Get the button element
+let button = document.getElementById("myButton");
+
+// Add an event listener to the button
+button.addEventListener("click", function () {
+    // Display an alert box with a message when the button is clicked
+    alert("You clicked the button!");
+});
+
---
+++ Jour6_Manipulation de chaînes/script.js
@@ -0,0 +1,9 @@
+// Jour 1: Fondamentaux
+/*
+Écrire une fonction pour vérifier si une chaîne est un palindrome.
+Implémenter une fonction pour compter le nombre de voyelles dans une chaîne.
+Créer une fonction pour inverser les mots dans une phrase.
+Écrire une fonction pour générer une chaîne aléatoire d'une longueur donnée.
+Implémenter une fonction pour vérifier si une chaîne est un anagramme d'une autre.
+*/
+
