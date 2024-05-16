583d7b94d9926aa2ef856522a3a85e0e3c870bdc
Rayane <rchaabane20@gmail.com>
2024-05-17 00:55:18 +0200

    Added directories for Day 3, Day 4, Day 5, Day 6, and Day 7

---
+++ Jour3_Fonctions_avancees/script.js
@@ -0,0 +1,8 @@
+// Jour 3
+/*
+1.Écrire une fonction curry pour créer une version partiellement appliquée d'une fonction.
+2.Implémenter une fonction récursive pour calculer la somme des chiffres d'un nombre.
+3.Créer une fonction qui prend une chaîne et renvoie un objet contenant le nombre d'occurrences de chaque caractère dans la chaîne.
+4.Écrire une fonction qui prend deux tableaux et renvoie un tableau contenant uniquement les éléments communs aux deux tableaux.
+5.Implémenter une fonction qui prend une fonction de rappel et l'exécute après un certain délai.
+*/
