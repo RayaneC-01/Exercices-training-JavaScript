7f36a8c328d495f038bd2a063d33eca129b95df1
Rayane <rchaabane20@gmail.com>
2024-05-17 01:21:57 +0200

    Jour 2 Fichier avec fonctions

--- Jour2_Manipulation_DOM/script.js
+++ Jour2_Manipulation_DOM/script.js
@@ -37,20 +37,44 @@
 
 
 // Liste à puces à partir d'un tableau d'éléments
-const cars = ["Volvo", "Toyota", "Kia", "Audi", "Ferrari", "Peugeot"];
-const ul = document.getElementById("myList");
+// const cars = ["Volvo", "Toyota", "Kia", "Audi", "Ferrari", "Peugeot"];
+// const ul = document.getElementById("myList");
 
-// Boucle sur le tableau d'éléments
-for (let i = 0; i < cars.length; i++) {
-    // Crée un élément de liste (li)
-    const li = document.createElement("li");
+// // Boucle sur le tableau d'éléments
+// for (let i = 0; i < cars.length; i++) {
+//     // Crée un élément de liste (li)
+//     const li = document.createElement("li");
 
-    // Ajoute le texte de l'élément de liste
-    li.appendChild(document.createTextNode(cars[i]));
+//     // Ajoute le texte de l'élément de liste
+//     li.appendChild(document.createTextNode(cars[i]));
 
-    // Ajoute l'élément de liste à la liste (ul)
-    ul.appendChild(li);
-}
+//     // Ajoute l'élément de liste à la liste (ul)
+//     ul.appendChild(li);
+// }
 
 //carrousel d'images simple qui change automatiquement toutes les quelques secondes
 
+// Get the slideshow element and its children (the figures)
+let slideshow = document.getElementById('slideshow');
+let figures = slideshow.children;
+
+// Initialize the current figure index
+let currentFigure = 0;
+
+/**
+ * Changes the opacity of the current figure to 0, increments the current figure index,
+ * and sets the opacity of the new current figure to 1.
+ */
+function nextFigure() {
+    // Change the opacity of the current figure to 0
+    figures[currentFigure].style.opacity = 0;
+
+    // Increment the current figure index
+    currentFigure = (currentFigure + 1) % figures.length;
+
+    // Set the opacity of the new current figure to 1
+    figures[currentFigure].style.opacity = 1;
+}
+
+// Change the figure every 5 seconds
+setInterval(nextFigure, 5000);
