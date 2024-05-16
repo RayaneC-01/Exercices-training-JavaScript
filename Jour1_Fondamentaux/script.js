8f6ec793a4b7f8f058f9e805f12aa3473a9731dc
Rayane <rchaabane20@gmail.com>
2024-05-16 18:39:24 +0200

    Entrainement exercice JS

---
+++ Jour1_Fondamentaux/script.js
@@ -0,0 +1,9 @@
+// Jour 1: Fondamentaux
+/*
+1. Écrire une fonction pour vérifier si un nombre est pair ou impair.
+2. Créer une fonction qui prend un tableau de nombres et renvoie la somme de ces nombres.
+3. Écrire une fonction pour convertir une chaîne en lettres majuscules.
+4. Créer une fonction pour calculer la factorielle d'un nombre.
+5. Écrire une fonction qui prend un tableau de nombres et renvoie le plus grand nombre de ce tableau.
+*/
+
