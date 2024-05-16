7d807d15fbc3f0da8d6fecbc7eb3f3a694fb0119
Rayane <rchaabane20@gmail.com>
2024-05-17 00:27:57 +0200

    Removed files using git rm

--- Jour 2_Manipulation DOM/script.js
+++
@@ -1,8 +0,0 @@
-// Jour 2
-/*
-1.Créer une page HTML avec un bouton. En utilisant JavaScript, ajoutez un événement clic au bouton qui affiche une boîte de dialogue avec un message.
-2.Écrire un script qui change la couleur de fond d'un élément de page lorsque l'utilisateur passe sa souris dessus.
-3.Créer un formulaire simple avec un champ de texte et un bouton. À la soumission du formulaire, récupérez la valeur du champ de texte et affichez-la dans la console.
-4.Construire une liste à puces à partir d'un tableau d'éléments à l'aide de JavaScript.
-5.Créer un carrousel d'images simple qui change automatiquement toutes les quelques secondes.
-*/
