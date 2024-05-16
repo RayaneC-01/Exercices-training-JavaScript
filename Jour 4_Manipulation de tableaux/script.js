8a9d15b7ca590e8d84246ed163aa15502f1802fb
Rayane <rchaabane20@gmail.com>
2024-05-16 18:40:08 +0200

    Entrainement exercice JS

---
+++ Jour 4_Manipulation de tableaux/script.js
@@ -0,0 +1,9 @@
+// Jour 4
+/*
+Écrire une fonction pour inverser un tableau.
+Implémenter une fonction de tri de tableau simple (par exemple, tri à bulles).
+Créer une fonction pour filtrer un tableau en fonction d'un critère donné.
+Écrire une fonction pour concaténer deux tableaux sans utiliser la méthode concat().
+Implémenter une fonction pour calculer la moyenne des nombres dans un tableau.
+*/
+
---
+++ Jour 5_AJAX et API/script.js
@@ -0,0 +1,13 @@
+// Jour 5
+/*
+Faire une requête AJAX vers une API publique (par exemple, l'API JSONPlaceholder) pour récupérer des données et les afficher dans la console.
+
+Créer une application météo simple qui utilise une API de météo (comme OpenWeatherMap) pour récupérer les données météorologiques en fonction de la ville saisie par l'utilisateur.
+
+Implémenter un système de pagination en récupérant des données d'une API qui renvoie un grand ensemble de données.
+
+Construire une application de liste de tâches qui utilise une API pour sauvegarder et récupérer les tâches.
+
+Créer un lecteur de flux RSS qui récupère et affiche les titres des articles à partir d'une source de flux donnée.
+*/
+
