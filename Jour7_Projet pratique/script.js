ef86a1907e12d235af4c4d226e3735398c1a910c
Rayane <rchaabane20@gmail.com>
2024-05-17 00:53:38 +0200

    Deleted files in Jour3_Fonctions avancées/, Jour4_Manipulation de tableaux/, Jour5_AJAX et API/, Jour6_Manipulation de chaînes/, and Jour7_Projet pratique/

--- Jour7_Projet pratique/script.js
+++
@@ -1,9 +0,0 @@
-// Jour 1: Fondamentaux
-/*
-Construire un jeu de pendu en JavaScript.
-Développer un gestionnaire de tâches qui permet aux utilisateurs de créer, éditer et supprimer des tâches.
-Créer un mini-jeu de mémoire qui affiche des cartes avec des images et demande aux joueurs de les associer.
-Implémenter une calculatrice simple avec des opérations de base (addition, soustraction, multiplication, division).
-Développer une application de chat en temps réel utilisant des technologies comme WebSocket ou Firebase Realtime Database.
-*/
-
