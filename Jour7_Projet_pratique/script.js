1bf22b9a4381c57dd3a99d9bdd181b3ab593995f
Rayane <rchaabane20@gmail.com>
2024-05-20 00:23:44 +0200

    Jour 7 Projet pratique

--- Jour7_Projet_pratique/script.js
+++ Jour7_Projet_pratique/script.js
@@ -8,57 +8,13 @@ Développer une application de chat en temps réel utilisant des technologies co
 */
 
 //Fonction pour Jeu de pendu
-let word = "javascript"; // The word to be guessed
-let guessedLetters = []; // Array to store guessed letters
-let attempts = 10; // Number of attempts allowed
-let wordArray = word.split(""); // Split the word into an array
-let [mask, setMask] = wordArray.map(() => "_"); // Create a mask array with underscores
+function pendu() {
+    let mot = "poulet";
+    let motCacher = mot.split("");
+    let motAfficher = motCacher.join("_");
+    let lettre = prompt("Entrez une lettre");
+    let lettreCacher = motCacher.join(" ");
+    let lettreAfficher = motCacher.join(" ");
+    let lettresTrouvees = new Array(50).fill(0);
 
-console.log("Let's play Hangman!"); // Display game start message
-console.log(mask.join(" ")); // Display masked word
-
-while (attempts > 0) {
-    let guess = prompt("Please guess a letter or type 'quit' to exit: ").toLowerCase(); // Get user input
-
-    if (guess === "quit") {
-        break; // Exit the game if user types 'quit'
-    }
-
-    if (guess.length !== 1) {
-        console.log("Please input only one letter at a time."); // Display error message if user inputs more than one letter
-        continue;
-    }
-
-    if (guessedLetters.includes(guess)) {
-        console.log("You already guessed this letter."); // Display error message if user guesses a letter already guessed
-        continue;
-    }
-
-    guessedLetters.push(guess); // Add guessed letter to guessedLetters array
-
-    let correctGuess = false;
-    for (let i = 0; i < wordArray.length; i++) {
-        if (wordArray[i] === guess) {
-            mask[i] = guess; // Replace underscore with guessed letter in mask array
-            correctGuess = true;
-        }
-    }
-
-    if (correctGuess) {
-        console.log("Correct guess!"); // Display success message if guessed letter is in the word
-    } else {
-        console.log("Incorrect guess."); // Display error message if guessed letter is not in the word
-        attempts--; // Decrement attempts
-    }
-
-    console.log(mask.join(" ")); // Display masked word with guessed letters
-
-    if (!mask.includes("_")) {
-        console.log("Congratulations, you won!"); // Display win message if all letters are guessed
-        break;
-    }
-
-    if (attempts === 0) {
-        console.log("Game over, the word was " + word); // Display loss message if attempts run out
-    }
 }
