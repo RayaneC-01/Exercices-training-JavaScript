db862cbfcbc0aa2951cfe57cedac5492fb58d917
Rayane <rchaabane20@gmail.com>
2024-05-21 19:01:20 +0200

    Jour 7 Projet pratique

--- Jour7_Projet_pratique/script.js
+++ Jour7_Projet_pratique/script.js
@@ -8,13 +8,3 @@ Développer une application de chat en temps réel utilisant des technologies co
 */
 
 //Fonction pour Jeu de pendu
-function pendu() {
-    let mot = "poulet";
-    let motCacher = mot.split("");
-    let motAfficher = motCacher.join("_");
-    let lettre = prompt("Entrez une lettre");
-    let lettreCacher = motCacher.join(" ");
-    let lettreAfficher = motCacher.join(" ");
-    let lettresTrouvees = new Array(50).fill(0);
-
-}
